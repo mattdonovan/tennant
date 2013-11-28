d58053ba4f5b9ea881a27bc6fbe2abd791170676
Matt Donovan <iam.mattdonovan@gmail.com>
2013-11-28 09:17:41 -0800

    starting the tennant project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tennant::Application.config.secret_token = '4a4c6f111cf5f0a3dc41538680bf5ae522b766e6cf58aa8c0512b1cc37c1cdd37b7d3cd1ecf8be3d4994478f7db138a17a7521581f949fbeba32d875ce04ad4b'
